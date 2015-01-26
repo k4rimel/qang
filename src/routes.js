80f4fb662666d62f6051e10d571779ecc6b06910
keara34 <karim.elallame@gmail.com>
2015-01-26 18:01:04 +0100

    formatting

---
+++ src/routes.js
@@ -0,0 +1,25 @@
+(function(){
+	'use strict';
+
+	angular.module('app')
+		.config(['$routeProvider','$locationProvider',
+			function($routeProvider, $locationProvider) {
+				$routeProvider
+					.when('/', {
+						templateUrl: 'src/views/partials/list.html',
+						controller: 'ListController'
+					})
+					.when('/player/:quizId', {
+						templateUrl: 'partials/player/player-start-screen.html',
+						controller: 'quizPlayerController'
+					})
+					.otherwise({
+						redirectTo: '/'
+					});
+					$locationProvider.html5Mode({
+						enabled: true,
+						requireBase: false
+					});
+				// add <base href=""> in <head> tag to the view if requireBase is set to true
+			}]);
+})();
